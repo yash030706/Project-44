26308340ed0ceee74259179506bcb1349a0e5fae
yash030706 <78734494+yash030706@users.noreply.github.com>
2021-05-23 10:47:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg")
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+
+  // Add code to display score in the middle of the screen
+    fill("white")
+    textSize(15)
+    text("Score: " + score, 800,50)
+    fill("lightblue")
+
+
+  // Add code to display the end screen
+  if(score === 3){
+    clear();
+    background(bg2);
+    fill("black")
+    textSize(40);
+    text("TREASURE UNLOCKED",250,200)
+  }
+
+
+  drawSprites()
+}
